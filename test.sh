518060a60ed1e4cbfdfafd6cd5669214a39b3207
Matt Bodholdt <mattb@gen6ventures.com>
2020-04-24 08:59:33 -0500

    update the test

--- test.sh
+++ test.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
-lookup=$(dig @127.0.0.1 one.one.one.one)
 
-if [ ! -z "$(echo "$lookup" | grep 'connection timed out; no servers could be reached')" ]; then
-  lookup2=$(dig @127.0.0.1 one.one.one.one)
-  if [ ! -z "$(echo "$lookup2" | grep 'connection timed out; no servers could be reached')" ]; then
-    procs=$(ps aux)
-    ns=$(netstat -anl)
-    echo "$procs"
-    echo "$ns"
-    echo "$lookup"
-    echo "$lookup2"
-    exit 1
-  fi
+dig @127.0.0.1 one.one.one.one +retry=3 +timeout=10 || fail="true"
+
+if [ "$fail" == "true" ]; then
+  procs=$(ps aux)
+  ns=$(netstat -anl)
+  echo "$procs"
+  echo "$ns"
+  echo "$lookup"
+  exit 1
 else
   echo "$lookup"
 fi
